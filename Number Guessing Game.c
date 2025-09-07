bc0acb26d0b5b5df1a8ebdbd7aba795daaef0a92
shantanu <i.am.shantanu07@gmail.com>
2025-09-07 18:38:33 +0530

    Update Number Guessing Game.c

--- Number Guessing Game.c
+++ Number Guessing Game.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 int main() {
-    printf("Welcome to the number guessing game!\nRULES:\nThe player must guess a number between 1 and 100.\nAfter each guess, the program will respond with:\n\t'Lower number please' if the guess is higher than the target number.\n\t'Higher number please' if the guess is lower than the target number.\nThe game continues until the player guesses the correct number.\nOn a correct guess, the program displays the total number of guesses taken.\nOnly integer inputs should be entered during guesses.");
+    printf("Welcome to the number guessing game!\nRULES:\nThe player must guess a number between 1 and 100.\nAfter each guess, the program will respond with:\n\t'Lower number please' if the guess is higher than the target number.\n\t'Higher number please' if the guess is lower than the target number.\nThe game continues until the player guesses the correct number.\nOn a correct guess, the program displays the total number of guesses taken.\nOnly integer inputs should be entered during guesses.\n");
     int number, guess, guesses = 0;
     // Initialize random number generator
     srand(time(NULL));
     // Generate random number between 1 and 100
     number = rand() % 100 + 1;
-    printf("Guess the number (between 1 and 100):\n");
+    printf("\nGuess the number (between 1 and 100):");
     // Loop until the correct guess
     do {
         scanf("%d", &guess);
@@ -25,4 +25,3 @@ int main() {
     printf("Thank You for playing!\nMade by Shantanu");
     return 0;
 }
-
